ec80877e4a1da85a2bc7e05367eecf794c41d78c
Geoffrey Hunter <gbmhunter@gmail.com>
2018-09-05 17:39:23 -0700

    Updated normal distribution PDF graphs.

--- Mathematics/Statistics/NormalDistribution/main.py
+++ Mathematics/Statistics/NormalDistribution/main.py
@@ -8,21 +8,23 @@ def normal_distribution_pdf(x, mean, sd):
     pd = scalar_fraction*math.exp(exp_fraction)
     return pd
 
+def add_nd_to_plot(mean, sd, ax):
+    x_a = np.arange(-10.0, 10.0, 0.01)
+    pdf_a = [ normal_distribution_pdf(x, mean, sd) for x in x_a ]
+    pdf_a = np.array(pdf_a)
+    points = ax.plot(x_a, pdf_a, label=f'mean={mean}, sd={sd}')
+
 if __name__ == '__main__':
 
-    MEAN = 0.0
-    SD = 1.0
-
-    x_a = np.arange(-10.0, 10.0, 0.01)
-    pdf_a = [ normal_distribution_pdf(x, MEAN, SD) for x in x_a ]
-    pdf_a = np.array(pdf_a)
+    MEANS = [0.0, 0.0, 3.0]
+    SDS = [1.0, 2.0, 0.5]
 
     fig, ax = plt.subplots()
+    for idx in range(len(MEANS)):
+        add_nd_to_plot(MEANS[idx], SDS[idx], ax)
 
-    points = ax.scatter(x_a, pdf_a, label='Data points')
-
-    plt.title('Normal Distribution PDF')
-    plt.xlabel('x')
-    plt.ylabel('y')
+    plt.title('Normal Distribution \nProbability Density Functions (PDFs)')
+    plt.xlabel('Random Variable')
+    plt.ylabel('PDF')
     ax.legend()
     plt.savefig('graph.png')
