33ac0650def594dd684aa96fa8bb29d11b1f06cc
Geoffrey Hunter <gbmhunter@gmail.com>
2018-09-24 14:38:29 -0700

    Added PyPoolProgress code.

---
+++ Programming/Languages/Python/PyPoolProgress/main.py
@@ -0,0 +1,43 @@
+import multiprocessing
+import time
+from random import randint
+
+CHECK_PERIOD_MS = 200
+PRINT_PERIOS_MS = 2000
+
+def worker(worker_id):
+    time.sleep(randint(1,4))
+    print(f'worker with id = {worker_id} finished.')
+
+def map_async_test(num_tasks):
+    pool = multiprocessing.Pool(processes=4)
+    rs = pool.map_async(worker, range(num_tasks))
+
+    print_counter = 0
+    while (True):
+        if (rs.ready()): break
+        remaining = rs._number_left
+        if print_counter >= PRINT_PERIOS_MS/CHECK_PERIOD_MS:
+            print("Waiting for", remaining, "tasks to complete...")
+            print_counter = 0
+        time.sleep(CHECK_PERIOD_MS/1000.0)
+        print_counter += 1
+
+    # result_worker_standard.wait()
+
+def imap_unordered_test(num_tasks):
+    pool = multiprocessing.Pool(processes=4)
+    rs = pool.imap_unordered(worker, range(num_tasks))
+    while (True):
+        completed = rs._index
+        if (completed == num_tasks): break
+        print(f'Waiting for {num_tasks-completed} tasks to complete...')
+        time.sleep(2)
+
+if __name__ == '__main__':
+    print('main() called.')
+    
+    map_async_test(10)
+    # imap_unordered_test(10)
+
+    print('main() finished.')
