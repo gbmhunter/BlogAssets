2d4ea50a9b860016cae749235011e79516226c91
Geoffrey Hunter <gbmhunter@gmail.com>
2019-01-30 22:22:40 -0800

    Adding graphing abilities to the serialization formats module.

--- Programming/serialization-formats/python/main.py
+++ Programming/serialization-formats/python/main.py
@@ -10,10 +10,11 @@ import toml
 import yaml
 import xml.etree.cElementTree as ET
 
-from dicttoxml import dicttoxml
-import xmltodict
+import matplotlib.pyplot as plt
+import numpy as np
 
-NUM_OBJECTS = 10000
+NUM_OBJECTS = 1000
+# NUM_OBJECTS = 10000
 
 def main():
     file_data = []
@@ -39,18 +40,44 @@ def main():
 
     timing_results_read = []
     for serial_format in serial_formats:
-        timing_results_read.append(measure_time(getattr(sys.modules[__name__], f'{serial_format}_read'), file_data))
+        timing_results_read.append(measure_time(getattr(sys.modules[__name__], f'{serial_format}_read')))
 
 
-    print(f'timing_results = {timing_results}')
+    print(f'timing_results_write = {timing_results_write}')
+    print(f'timing_results_read = {timing_results_read}')
+
+    ####################################################################################################
+    # DRAW GRAPHS
+    ####################################################################################################
+
+    bar_width = 0.35
+    x_positions = np.arange(0, len(serial_formats))
+    fig, ax = plt.subplots()
+
+    print(x_positions)
+    print(timing_results_write)
+    ax.bar(x_positions, timing_results_write, width=bar_width)
+    ax.bar(x_positions + bar_width, timing_results_read, width=bar_width)
+
+    ax.set_xlabel('Serialization Format')
+    ax.set_xticks(x_positions)
+    ax.set_xticklabels(serial_formats)
+
+    ax.set_ylabel('Seconds (s)')
+
+    ax.set_title('File Write/Read Times For Popular Serialization Formats')
+
+    plt.show()
+
 
 def wrapper(func, *args, **kwargs):
     def wrapped():
         return func(*args, **kwargs)
     return wrapped
 
-def measure_time(func, arguments):
-    wrapped = wrapper(func, arguments)
+def measure_time(func, *arguments):
+    wrapped = wrapper(func, *arguments)
+    # We only want to include one run in the time info
     return timeit.timeit(wrapped, number=1)
 
 def csv_write(file_data):
@@ -58,8 +85,8 @@ def csv_write(file_data):
     with open('data.csv', 'w') as file:
         csv_writer = csv.writer(file, delimiter=',')
         # Write header row
-        csv_writer.writerow(file_data['people'][0].keys())
-        for person in file_data['people']:
+        csv_writer.writerow(file_data[0].keys())
+        for person in file_data:
             csv_writer.writerow(person.values())
 
 def csv_read():
@@ -85,7 +112,7 @@ def json_read():
 def toml_write(file_data):
     print(f'Writing .toml file.')
     with open('data.toml', 'w') as file:
-        toml.dump(file_data, file)
+        toml.dump({ 'data': file_data }, file)
 
 def toml_read():
     print(f'Reading .toml file.')
@@ -106,16 +133,29 @@ def yaml_read():
 
 def xml_write(file_data):
     print(f'Writing .xml file.')
-    print(file_data)
-    with open('data.xml', 'w') as file:
-        xml = xmltodict.unparse(file_data)
-        file.write(xml.decode())
+
+    xml_people = ET.Element('people')
+    for person in file_data:
+        xml_person = ET.SubElement(xml_people, 'person')
+        for key, value in person.items():
+            xml_element = ET.SubElement(xml_person, str(key))
+            xml_element.text = str(value)
+
+    tree = ET.ElementTree(xml_people)
+    tree.write('data.xml')
 
 def xml_read():
-    print(f'Reading .xml file.')
-    with open('data.xml', 'r') as file:
-        data = xmltodict.parse(file.read())
-    return data
+    tree = ET.parse('data.xml')
+    xml_people = tree.getroot()
+
+    people = []
+    for xml_person in xml_people:
+        person = {}
+        for xml_person_property in xml_person:
+            person[xml_person_property.tag] = xml_person_property.text
+        people.append(person)
+
+    return people
 
 def string_generator(size=6, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
